e594e3c43be4d8e9073aae667edc61512191c000
BorislavRaynov <b_raynov@abv.bg>
2024-05-18 13:37:25 +0300

    Article model added.

--- dataScrap/data_scrap_api/models.py
+++ dataScrap/data_scrap_api/models.py
@@ -1,2 +1,12 @@
 from django.db import models
 
+
+class Article(models.Model):
+    uid = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    url = models.URLField(unique=True)
+    publication_date = models.DateTimeField()
+    author = models.CharField(max_length=255)
+    image_urls = models.JSONField()
+    entities = models.JSONField()

d2e699aa10c24be247ea27465246848b39a7ffb7
BorislavRaynov <b_raynov@abv.bg>
2024-05-21 20:26:27 +0300

    Selectors fixed.

--- articles_scraper/spiders/capitalbrief.py
+++ articles_scraper/spiders/capitalbrief.py
@@ -7,20 +7,19 @@ class CapitalBriefSpider(scrapy.Spider):
     start_urls = ['https://www.capitalbrief.com/technology/']
 
     def parse(self, response, *args, **kwargs):
-        articles = response.css('article')
-        for article in articles:
-            article_url = article.css('a::attr(href)').get()
-            if article_url:
-                yield response.follow(article_url, self.parse_article)
 
-            else:
-                next_page = response.css('a.btn-more::attr(href)').get()
-                if next_page:
-                    yield response.follow(next_page, self.parse)
+        articles = response.css("article")
+        for article in articles:
+            article_url = article.css("h2 a::attr(href)").get()
+            yield response.follow(article_url, self.parse_article)
+
+        next_page = response.css('a.btn-more::attr(href)').get()
+        if next_page is not None:
+            yield response.follow(next_page, self.parse)
 
     def parse_article(self, response):
         title = response.css('h1::text').get()
-        body = response.css('section p::text').get()
+        body = response.css('div.content p::text').get()
         url = response.url
         publication_date_str = response.css('time::attr(datetime)').get()
         author = response.css('.author::text').get()
@@ -29,6 +28,7 @@ class CapitalBriefSpider(scrapy.Spider):
         title = title.strip()
         body = body.strip()
         author = author.replace('\xa0', ' ').strip()
+
         if image_urls:
             image_urls = [url.strip() for url in image_urls]
 

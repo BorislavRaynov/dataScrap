ee146b6de93cfc5cad1216362ce2de893c984910
BorislavRaynov <b_raynov@abv.bg>
2024-05-22 06:14:02 +0300

    Static dirs added.

--- dataScrap/settings.py
+++ dataScrap/settings.py
@@ -90,5 +90,10 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    BASE_DIR / "static/css",
+    BASE_DIR / "static/js",
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
